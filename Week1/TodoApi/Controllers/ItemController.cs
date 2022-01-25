a0f7af20da6e03910ad327fd29465c67a21abe08
Oğuzhan Kuşcu <oguzk17@gmail.com>
2022-01-25 23:40:33 +0300

    init

---
+++ Week1/TodoApi/Controllers/ItemController.cs
@@ -0,0 +1,107 @@
+﻿
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApi.Models;
+
+
+namespace TodoApi.Controllers
+{
+
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ItemController : ControllerBase
+	{
+
+		// test için değişkenler tanımlandı
+		public static List<Items> items = new List<Items>()
+																						{
+
+				new Items { Tckn = 1, Name = "Onur", Surname = "Kuşcu",   Age =24       },
+				new Items { Tckn = 2, Name = "Oguzhan", Surname = "Kuşcu"     ,Age =24   },
+				new Items { Tckn = 3, Name = "Çiğdem", Surname = "Kuşcu"  ,Age =24   },
+				new Items { Tckn = 4, Name = "Nuray", Surname = "Kuşcu "   , Age =24 },
+				new Items { Tckn = 5, Name = "Mecit", Surname = "Kuşcu"     ,Age =24  }
+																					 };
+
+		//GETALL methodu 
+		[HttpGet]
+		public ActionResult<IEnumerable<Items>> GetAllItems()
+		{
+
+			return items;
+		}
+
+		//Id ye göre filtrelemek için
+		[HttpGet("{id}")]
+		public ActionResult<Items> Get(int id)
+		{
+			// verilen id ye göre listeyi tarar
+			var item = items.FirstOrDefault(x => x.Tckn == id);
+			if (item == null)
+			{
+				return NotFound("Aranılan kisi bulunamadı");
+			}
+			return item;
+		}
+
+		//Ekleme işlemi yapılıyor.
+		[HttpPost]
+		public IActionResult Post([FromBody] Items item)
+		{
+			items.Add(item);
+			return StatusCode(StatusCodes.Status201Created);
+		}
+
+
+
+		// UPDATE  işlemi yapılıyor
+		[HttpPut("{id}")]
+		public IActionResult Put(int id, [FromBody] Items item)
+		{
+			var currentItem = items.Where(x => x.Tckn == id).FirstOrDefault();
+
+			if (currentItem != null)
+			{
+				currentItem.Name = item.Name;
+				currentItem.Name = item.Name;
+
+
+				return Ok("İslem Basarılı");
+			}
+			else
+			{
+				return NotFound("Kisi bulunamadı!");
+			}
+		}
+
+		//Delete işlemi yapılıyor.
+		[HttpDelete("{id}")]
+		public ActionResult<Items> Delete(int id)
+		{
+			var item = items.FirstOrDefault(x => x.Tckn == id);
+			if (item != null)
+			{
+				items.Remove(item);
+				return Ok("İslem Basarılı");
+			}
+			else
+			{
+				return NotFound("Aradığınız kişi bulunamadı");
+			}
+
+
+
+
+		}
+
+
+	}
+
+}
+
+
---
+++ Week1/TodoApi/Model/Items.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace TodoApi.Models
+{
+
+	public class Items
+	{
+		private int tckn;
+		private string isim;
+		private string soyisim;
+		private int yas;
+
+		public int Tckn
+		{
+			get { return this.tckn; }
+			set { this.tckn = value; }
+		}
+		public string Name
+		{
+			get { return this.Name; }
+			set { this.Name = value; }
+		}
+		public string Surname
+		{
+			get { return this.Surname; }
+			set { this.Surname = value; }
+		}
+
+		public int Age
+		{
+			get { return this.Age; }
+			set { this.Age = value; }
+		}
+		public Items()
+		{
+			this.Tckn = tckn;
+			this.Name = Name;
+			this.Surname = Surname;
+			this.Age = Age;
+		}
+	}
+
+}
